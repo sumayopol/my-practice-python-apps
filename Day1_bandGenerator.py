3d9a6e9db1da20b8c3ed7168e54324ddf0e95df6
iStarC_Admin1 <pol.sumayo@istarc.io>
2024-12-11 06:35:44 +0800

    Final versions of these files

--- Day1_bandGenerator.py
+++ Day1_bandGenerator.py
@@ -6,4 +6,5 @@ pet = input("What is the name of your pet?: \n")
 
 print("Your band name could be: " + city + " " + pet)
 
+
 #print(f"Your band name could be: {city} {pet}") # another possible way of displaying the message
